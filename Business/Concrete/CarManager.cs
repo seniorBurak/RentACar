debb181e7b841cc0e60c528ad20372b5512a0571
makina <makina@DESKTOP-R39P48C>
2022-08-30 13:23:37 +0300

    30.8

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -23,5 +23,15 @@ namespace Business.Concrete
         {
             return _carDal.GetAll();
         }
+
+        public List<Car> GetAllByBrandId(int id)
+        {
+            return _carDal.GetAll(c => c.BrandId == id);
+        }
+
+        public List<Car> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _carDal.GetAll(c=>c.DailyPrice == min && c.DailyPrice == max);   
+        }
     }
 }
---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,62 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : ICarDal
+    {
+        public void Add(Car entity)
+        {
+           using(RecapProjectContext context = new RecapProjectContext())
+            {
+                var addedEntity = context.Entry(entity);
+                addedEntity.State=Microsoft.EntityFrameworkCore.EntityState.Added;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(Car entity)
+        {
+            using (RecapProjectContext context = new RecapProjectContext())
+            {
+                var deletedEntity = context.Entry(entity);
+                deletedEntity.State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+                context.SaveChanges();
+            }
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            using(RecapProjectContext context = new RecapProjectContext())
+            {
+                return context.Set<Car>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            using (RecapProjectContext context = new RecapProjectContext())
+            {
+                return filter == null
+                    ? context.Set<Car>().ToList()
+                    : context.Set<Car>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Car entity)
+        {
+            using (RecapProjectContext context = new RecapProjectContext())
+            {
+                var updatedEntity = context.Entry(entity);
+                updatedEntity.State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+    }
+}
